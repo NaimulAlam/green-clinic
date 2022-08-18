0e5cc8fe556fd001e2224f1e20e1cfcf3ca89318
Mohmammad Naimul Alam <naimul2707@gmail.com>
2022-08-18 23:46:44 +0600

    login page basic form with react hook form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
 
   if (error) {
     console.error("err", error);
@@ -19,6 +27,52 @@ const Login = () => {
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="text-center font-bold text-2xl">Login</h2>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Your Email"
+                className="input input-bordered w-full max-w-xs"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is required",
+                  },
+                  pattern: {
+                    value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                    message: "Email is not valid",
+                  },
+                })}
+              />
+              <label className="label">
+                <span className="label-text-alt">Alt Email label</span>
+              </label>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Your Password"
+                className="input input-bordered w-full max-w-xs"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "password is required",
+                  },
+                })}
+              />
+              <label className="label">
+                <span className="label-text-alt">Alt Pass label</span>
+              </label>
+            </div>
+          </form>
+
           <div className="divider">OR</div>
           <button
             onClick={() => signInWithGoogle()}
