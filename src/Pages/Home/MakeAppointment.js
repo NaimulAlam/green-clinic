a3cad35c3272bdce59ff0265938c1308cffc82a4
Mohmammad Naimul Alam <naimul2707@gmail.com>
2022-08-24 23:59:47 +0600

    home page btn link corrections

--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import doctor from '../../assets/images/doctor.png';
-import apointmentBG from '../../assets/images/appointment.png';
-import PrimaryBtn from '../Shared/PrimaryBtn';
+import React from "react";
+import doctor from "../../assets/images/doctor.png";
+import apointmentBG from "../../assets/images/appointment.png";
+import PrimaryBtn from "../Shared/PrimaryBtn";
+import { Link } from "react-router-dom";
 
 const MakeAppointment = () => {
   return (
@@ -12,7 +13,11 @@ const MakeAppointment = () => {
       className="flex justify-center items-center my-12"
     >
       <div className="flex-1 md:visible invisible">
-        <img className='md:mt-[-100px] lg:mt-[-150px]' src={doctor} alt="doctor" />
+        <img
+          className="md:mt-[-100px] lg:mt-[-150px]"
+          src={doctor}
+          alt="doctor"
+        />
       </div>
       <div className="md:flex-1 px-5 py-10">
         <h3 className="text-xl text-primary uppercase font-bold">Apointment</h3>
@@ -23,7 +28,9 @@ const MakeAppointment = () => {
           necessitatibus. Corrupti quia ratione fuga quas, maiores et natus
           minus praesentium molestiae nemo.
         </p>
-        <PrimaryBtn>Book Now</PrimaryBtn>
+        <Link to="appointment">
+          <PrimaryBtn>Book Now</PrimaryBtn>
+        </Link>
       </div>
     </section>
   );
